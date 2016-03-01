c4b0724e0c87784e8c5a0e5364744588469a10f1
williamdazet <wdazet2@lsu.edu>
2016-03-01 10:23:51 -0600

    Update script.js

--- demo/js/script.js
+++ demo/js/script.js
@@ -3,14 +3,14 @@ var map = L.map('map').setView([30, -90], 4);
 var streets = L.tileLayer('http://{s}.tile.osm.org/{z}/{x}/{y}.png', {
     attribution: '&copy; <a href="http://osm.org/copyright">OpenStreetMap</a> contributors'
 }).addTo(map);
-// Added lighting
+// Added lighting effects to map to see where lightning is occuring 
 var lightning = L.tileLayer.wms("http://nowcoast.noaa.gov/arcgis/services/nowcoast/sat_meteo_emulated_imagery_lightningstrikedensity_goes_time/MapServer/WMSServer", {
     layers: '1',
     format: 'image/png',
     transparent: true,
     attribution: "NOAA"
 }).addTo(map);
-//Added meteohydro
+//Added meteohydro to see rain storm and opacity to see through to map
 var meteohydro = L.tileLayer.wms("http://nowcoast.noaa.gov/arcgis/services/nowcoast/analysis_meteohydro_sfc_rtma_time/MapServer/WMSServer", {
     layers: '1',
     format: 'image/png',
@@ -18,7 +18,7 @@ var meteohydro = L.tileLayer.wms("http://nowcoast.noaa.gov/arcgis/services/nowco
     opacity: 0.333,
     attribution: "NOAA"
 }).addTo(map);
-// Added daily max air temp.
+// Added daily max air temp. and opacity to see through onto map
 var maxtemp = L.tileLayer.wms("http://nowcoast.noaa.gov/arcgis/services/nowcoast/forecast_meteoceanhydro_sfc_ndfd_dailymaxairtemp_offsets/MapServer/WMSServer", {
     layers: '1',
     format: 'image/png',
