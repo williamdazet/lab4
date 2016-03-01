d9fbbbe8ecc740f4e1686da8b1c8b8928abe11cb
williamdazet <wdazet2@lsu.edu>
2016-03-01 10:14:37 -0600

    Added files via upload

--- demo/js/script.js
+++ demo/js/script.js
@@ -1,16 +1,16 @@
-//lat and lon coordinates 
+// create map object and center lat/lon and zoom level
 var map = L.map('map').setView([30, -90], 4);
 var streets = L.tileLayer('http://{s}.tile.osm.org/{z}/{x}/{y}.png', {
     attribution: '&copy; <a href="http://osm.org/copyright">OpenStreetMap</a> contributors'
 }).addTo(map);
-//Added lightning detection 
-var lighting = L.tileLayer.wms("http://nowcoast.noaa.gov/arcgis/services/nowcoast/sat_meteo_emulated_imagery_lightningstrikedensity_goes_time/MapServer/WMSServer", {
+// Added lighting
+var lightning = L.tileLayer.wms("http://nowcoast.noaa.gov/arcgis/services/nowcoast/sat_meteo_emulated_imagery_lightningstrikedensity_goes_time/MapServer/WMSServer", {
     layers: '1',
     format: 'image/png',
     transparent: true,
     attribution: "NOAA"
 }).addTo(map);
-//Added analysis meteohydro 
+//Added meteohydro
 var meteohydro = L.tileLayer.wms("http://nowcoast.noaa.gov/arcgis/services/nowcoast/analysis_meteohydro_sfc_rtma_time/MapServer/WMSServer", {
     layers: '1',
     format: 'image/png',
@@ -18,7 +18,7 @@ var meteohydro = L.tileLayer.wms("http://nowcoast.noaa.gov/arcgis/services/nowco
     opacity: 0.333,
     attribution: "NOAA"
 }).addTo(map);
-//Added daily max air temp  
+// Added daily max air temp.
 var maxtemp = L.tileLayer.wms("http://nowcoast.noaa.gov/arcgis/services/nowcoast/forecast_meteoceanhydro_sfc_ndfd_dailymaxairtemp_offsets/MapServer/WMSServer", {
     layers: '1',
     format: 'image/png',
@@ -26,6 +26,7 @@ var maxtemp = L.tileLayer.wms("http://nowcoast.noaa.gov/arcgis/services/nowcoast
     opacity: 0.222,
     attribution: "NOAA"
 }).addTo(map);
+
 //create an object with layers for each base map
 var baseLayers = {
     "streets": streets,
